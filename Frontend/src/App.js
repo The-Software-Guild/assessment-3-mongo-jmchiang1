15fe8b056a6c6cd3a0a08a4278b27e2188360506
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-24 00:28:49 -0500

    connected react to map out users to page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const getData = async () => {
+    const res = await axios.get("/api/users");
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Testing</h1>
+    <>
+    <h1>BUG TRACKER 3000</h1>
+    <div className="container">
+      {users.map((user) => {
+        return (
+          <div key={user._id} className="users">
+            <h2>Name: {user.name} </h2>
+            <h2>Email: {user.email} </h2>
+          </div>
+        );
+      })}
     </div>
+    </>
   );
-}
+};
 
 export default App;
-
