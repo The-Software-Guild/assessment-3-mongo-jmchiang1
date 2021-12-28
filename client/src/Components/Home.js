9342964a88c94f57993d0664a6f256f84c2eab44
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-28 13:52:41 -0500

    clean slate

--- client/src/Components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Home = () => (
-  <div style={{display: 'flex', justifyContent: "center", margin: '15rem'}}>
-    <h1>Welcome to my bug tracker!</h1>
-  </div>
-);
-
-export default Home;
