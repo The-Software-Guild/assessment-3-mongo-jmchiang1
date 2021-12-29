25a5ed6ec23a72136f25d78b4b4f04935ee3bae5
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-29 12:48:50 -0500

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+// import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Testing</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+    <Routes>
+      {/* <Route path="/" element={<App />} /> */}
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
